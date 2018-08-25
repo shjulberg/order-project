60138d9ea0d21415b505fa12565cb04d86f285dc
Shane Hjulberg <shjulberg@gmail.com>
2018-08-24 23:01:32 -0500

    Added ngBootstrap for formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { OrderDataComponent } from '../Orders/order-data.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
+
     AppComponent,
     OrderDataComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
